970c1cdc03481e9ef5eaca3707121b7985dac576
valjuniel123 <valjuniel123@gmail.com>
2023-07-02 22:14:05 +0800

    Error Modification

--- account/model/accountHandlerModel.php
+++ account/model/accountHandlerModel.php
@@ -45,7 +45,7 @@ function registerAccount($data)
     }
     
     $sql = "INSERT INTO account (password, email, account_type, access_level) VALUES ('" . $data['password'] . "', '" . $data['email'] . "', 3, 0)";
-    $query = mysqli_query($conn, $sql) or die("Error RC001: " . mysqli_error($conn));
+    $query = mysqli_query($conn, $sql) or die("Error RQ001: " . mysqli_error($conn));
 
     if ($query)
     {
@@ -53,12 +53,12 @@ function registerAccount($data)
 
         $sql = "INSERT INTO user_info (account_id, first_name, middle_name, last_name, suffix, birth_date, birth_place, address_line, barangay, municipality, province, region, religion, gender, civil_status, contact_number)
         VALUES ('$last_id', '$data[firstName]', '$data[middleName]', '$data[lastName]', '$data[suffix]', '$data[birthdate]', '$data[birthPlace]', '$data[address]', '$data[barangay]', '$data[city]', '$data[province]', '$data[region]', '$data[religion]', '$data[gender]', '$data[civilStatus]', '$data[contactNo]')";
-        $query = mysqli_query($conn, $sql) or die("Error RC002: " . mysqli_error($conn));
+        $query = mysqli_query($conn, $sql) or die("Error RQ002: " . mysqli_error($conn));
 
         if ($query)
         {
             $sql = "INSERT INTO email_token (user_id, email, token, date_generated) VALUES ('$last_id', '$data[email]', '" . generateRandomString(5) . "', NOW())";
-            $query = mysqli_query($conn, $sql) or die("Error RC003: " . mysqli_error($conn));
+            $query = mysqli_query($conn, $sql) or die("Error RQ003: " . mysqli_error($conn));
 
             if ($query)
             {
@@ -66,17 +66,17 @@ function registerAccount($data)
             }
             else
             {
-                echo 'Error RC004: ' . mysqli_error($conn);
+                echo 'Error RQ004: ' . mysqli_error($conn);
             }
         }
         else
         {
-            echo 'Error RC003: ' . mysqli_error($conn);
+            echo 'Error RQ003: ' . mysqli_error($conn);
         }
     }
     else
     {
-        echo 'Error RC002: ' . mysqli_error($conn);
+        echo 'Error RQ002: ' . mysqli_error($conn);
     }
 }
 
@@ -108,7 +108,7 @@ function email_confirmation($data)
 
         if ($timeLeft <= 0)
         {
-            return 'Error EC003: Token Expired';
+            return 'Error EQ003: Token Expired';
         }
         else
         {
@@ -124,12 +124,12 @@ function email_confirmation($data)
             }
             else
             {
-                echo 'Error EC002: ' . $conn->error;
+                echo 'Error EQ002: ' . $conn->error;
             }
         }
     }
     else
     {
-        echo 'Error EC001: ' . $conn->error;
+        echo 'Error EQ001: ' . $conn->error;
     }
 }
