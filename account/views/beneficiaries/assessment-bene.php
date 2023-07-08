32cb856372e94fa4fd54c5e442112a6353a92c48
angeliV13 <angelireyes2213@gmail.com>
2023-07-08 21:30:20 +0800

    assessment

--- account/views/beneficiaries/assessment-bene.php
+++ account/views/beneficiaries/assessment-bene.php
@@ -3,22 +3,12 @@
   <div class="column">
     <div class="card">
         <div class="header-group mb-3">
-          <h5 class="card-header bg-danger d-flex justify-content-center" style="color:white">Assessment Requirements</h5>
+          <h5 class="card-header bg-primary d-flex justify-content-center" style="color:white">Assessment Requirements</h5>
         </div>
         <div class="card-body">
-          <div class="dropdown py-2">
-            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Academic Year
-            </button>
-           <div class="dropdown-menu">
-              <a class="dropdown-item" href=" ">Regular link</a>
-              <a class="dropdown-item active" href=" ">Active link</a>
-              <a class="dropdown-item" href=" ">Another link</a>
-            </div>
-          </div>
           <div class= "table-responsive">
               <!-- Table with stripped rows -->
-              <table class="table  table-bordered table-hover"  id="viewAdmin" width="100%" cellspacing="0" >
+              <table class="table table-bordered table-hover"  id="viewAdmin" width="100%" cellspacing="0" >
                 <thead>
                   <tr class="text-center">
                     <th>No</th>
@@ -32,7 +22,7 @@
                   <tr>
                       <td>1</td>
                       <td>School ID</td>
-                      <td>brandon@gmail.com</td>
+                      <td>Approved/Rejected</td>
                       <td>BSU </td>
                       <td>
                         <div class="btn-group-vertical d-flex" >
@@ -42,23 +32,122 @@
                               <div class="modal-dialog modal-dialog-scrollable">
                                 <div class="modal-content">
                                   <div class="modal-header">
-                                    <h5 class="modal-title">Modal Dialog Scrollable</h5>
+                                    <h5 class="modal-title">School ID</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                   </div>
                                   <div class="modal-body">
-                                    Non omnis incidunt qui sed occaecati magni asperiores est mollitia. Soluta at et reprehenderit. Placeat autem numquam et fuga numquam. Tempora in facere consequatur sit dolor ipsum. Consequatur nemo amet incidunt est facilis. Dolorem neque recusandae quo sit molestias sint dignissimos.
-                                    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-                                    This content should appear at the bottom after you scroll.
+                                    <div class="file-loading">
+                                      <input id="input-b7" name="input-b7[]" multiple type="file" class="file" data-allowed-file-extensions='["csv", "txt"]'>
+                                    </div>
                                   </div>
                                   <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                    <button type="button" class="btn btn-primary">Save</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="Delete">Delete</button>
                                   </div>
                                 </div>
                               </div>
                             </div>
                           <button class="btn btn-dark" data-bs-toggle="modal">Not Applicable</button>
-                          <button class="btn btn-warning" data-bs-toggle="modal">Save</button>
+                          <!-- <button class="btn btn-warning" data-bs-toggle="modal">Save</button> -->
+                          <!-- <button class="btn btn-warning" data-toggle="modal" data-target="#editRoomModal">Edit</button>
+                          <button class="btn btn-danger" data-toggle="modal" data-target="#deleteRoomModal">Delete</button> -->
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>School Clearance</td>
+                      <td>Approved/Rejected</td>
+                      <td>BSU </td>
+                      <td>
+                        <div class="btn-group-vertical d-flex" >
+                          <!-- <button class="btn btn-primary" data-bs-toggle="modal">View File</button> -->
+                          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDialogScrollable"> View File</button>
+                            <div class="modal fade" id="modalDialogScrollable" tabindex="-1">
+                              <div class="modal-dialog modal-dialog-scrollable">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title">School ID</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                  </div>
+                                  <div class="modal-body">
+                                    
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary">Save</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="Delete">Delete</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          <button class="btn btn-dark" data-bs-toggle="modal">Not Applicable</button>
+                          <!-- <button class="btn btn-warning" data-bs-toggle="modal">Save</button> -->
+                          <!-- <button class="btn btn-warning" data-toggle="modal" data-target="#editRoomModal">Edit</button>
+                          <button class="btn btn-danger" data-toggle="modal" data-target="#deleteRoomModal">Delete</button> -->
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Certificate of Registration</td>
+                      <td>Approved/Rejected</td>
+                      <td>BSU </td>
+                      <td>
+                        <div class="btn-group-vertical d-flex" >
+                          <!-- <button class="btn btn-primary" data-bs-toggle="modal">View File</button> -->
+                          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDialogScrollable"> View File</button>
+                            <div class="modal fade" id="modalDialogScrollable" tabindex="-1">
+                              <div class="modal-dialog modal-dialog-scrollable">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title">School ID</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                  </div>
+                                  <div class="modal-body">
+                                    
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary">Save</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="Delete">Delete</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          <button class="btn btn-dark" data-bs-toggle="modal">Not Applicable</button>
+                          <!-- <button class="btn btn-warning" data-bs-toggle="modal">Save</button> -->
+                          <!-- <button class="btn btn-warning" data-toggle="modal" data-target="#editRoomModal">Edit</button>
+                          <button class="btn btn-danger" data-toggle="modal" data-target="#deleteRoomModal">Delete</button> -->
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>Grade Report</td>
+                      <td>Approved/Rejected</td>
+                      <td>BSU </td>
+                      <td>
+                        <div class="btn-group-vertical d-flex" >
+                          <!-- <button class="btn btn-primary" data-bs-toggle="modal">View File</button> -->
+                          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDialogScrollable"> View File</button>
+                            <div class="modal fade" id="modalDialogScrollable" tabindex="-1">
+                              <div class="modal-dialog modal-dialog-scrollable">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title">School ID</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                  </div>
+                                  <div class="modal-body">
+                                    
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary">Save</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="Delete">Delete</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          <button class="btn btn-dark" data-bs-toggle="modal">Not Applicable</button>
+                          <!-- <button class="btn btn-warning" data-bs-toggle="modal">Save</button> -->
                           <!-- <button class="btn btn-warning" data-toggle="modal" data-target="#editRoomModal">Edit</button>
                           <button class="btn btn-danger" data-toggle="modal" data-target="#deleteRoomModal">Delete</button> -->
                         </div>
--- account/views/beneficiaries/renewal-bene.php
+++ account/views/beneficiaries/renewal-bene.php
@@ -3,117 +3,96 @@
   <div class="column">
     <div class="card">
         <div class="header-group mb-3">
-          <h5 class="card-header bg-primary d-flex justify-content-center" style="color:white">Assessment Requirements</h5>
+          <h5 class="card-header bg-primary d-flex justify-content-center" style="color:white">Renewal Requirements</h5>
         </div>
         <div class="card-body">
-          
           <div class= "table-responsive">
               <!-- Table with stripped rows -->
-              <table id="table" class="display responsive nowrap " style="width:100%">
-        <thead>
-            <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-                <th>Extn.</th>
-                <th>E-mail</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Jennifer</td>
-                <td>Acosta</td>
-                <td>Junior Javascript Developer</td>
-                <td>Edinburgh</td>
-                <td>43</td>
-                <td>2013-02-01</td>
-                <td>$75,650</td>
-                <td>3431</td>
-                <td>j.acosta@datatables.net</td>
-            </tr>
-            <tr>
-                <td>Cara</td>
-                <td>Stevens</td>
-                <td>Sales Assistant</td>
-                <td>New York</td>
-                <td>46</td>
-                <td>2011-12-06</td>
-                <td>$145,600</td>
-                <td>3990</td>
-                <td>c.stevens@datatables.net</td>
-            </tr>
-            <tr>
-                <td>Hermione</td>
-                <td>Butler</td>
-                <td>Regional Director</td>
-                <td>London</td>
-                <td>47</td>
-                <td>2011-03-21</td>
-                <td>$356,250</td>
-                <td>1016</td>
-                <td>h.butler@datatables.net</td>
-            </tr>
-            <tr>
-                <td>Lael</td>
-                <td>Greer</td>
-                <td>Systems Administrator</td>
-                <td>London</td>
-                <td>21</td>
-                <td>2009-02-27</td>
-                <td>$103,500</td>
-                <td>6733</td>
-                <td>l.greer@datatables.net</td>
-            </tr>
-            <tr>
-                <td>Jonas</td>
-                <td>Alexander</td>
-                <td>Developer</td>
-                <td>San Francisco</td>
-                <td>30</td>
-                <td>2010-07-14</td>
-                <td>$86,500</td>
-                <td>8196</td>
-                <td>j.alexander@datatables.net</td>
-            </tr>
-            <tr>
-                <td>Shad</td>
-                <td>Decker</td>
-                <td>Regional Director</td>
-                <td>Edinburgh</td>
-                <td>51</td>
-                <td>2008-11-13</td>
-                <td>$183,000</td>
-                <td>6373</td>
-                <td>s.decker@datatables.net</td>
-            </tr>
-            <tr>
-                <td>Michael</td>
-                <td>Bruce</td>
-                <td>Javascript Developer</td>
-                <td>Singapore</td>
-                <td>29</td>
-                <td>2011-06-27</td>
-                <td>$183,000</td>
-                <td>5384</td>
-                <td>m.bruce@datatables.net</td>
-            </tr>
-            <tr>
-                <td>Donna</td>
-                <td>Snider</td>
-                <td>Customer Support</td>
-                <td>New York</td>
-                <td>27</td>
-                <td>2011-01-25</td>
-                <td>$112,000</td>
-                <td>4226</td>
-                <td>d.snider@datatables.net</td>
-            </tr>
-        </tbody>
-    </table>
+              <table class="table table-bordered table-hover"  id="viewAdmin" width="100%" cellspacing="0" >
+                <thead>
+                  <tr class="text-center">
+                    <th>No</th>
+                    <th>Requirements</th>
+                    <th>Status</th>
+                    <th>Remarks</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                      <td>1</td>
+                      <td>School ID</td>
+                      <td>Approved/Rejected</td>
+                      <td>BSU </td>
+                      <td>
+                        <div class="btn-group-vertical d-flex" >
+                          <!-- <button class="btn btn-primary" data-bs-toggle="modal">View File</button> -->
+                          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDialogScrollable"> View File</button>
+                            <div class="modal fade" id="modalDialogScrollable" tabindex="-1">
+                              <div class="modal-dialog modal-dialog-scrollable">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title">School ID</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                  </div>
+                                  <div class="modal-body">
+                                    
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary">Save</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="Delete">Delete</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          <button class="btn btn-dark" data-bs-toggle="modal">Not Applicable</button>
+                          <!-- <button class="btn btn-warning" data-bs-toggle="modal">Save</button> -->
+                          <!-- <button class="btn btn-warning" data-toggle="modal" data-target="#editRoomModal">Edit</button>
+                          <button class="btn btn-danger" data-toggle="modal" data-target="#deleteRoomModal">Delete</button> -->
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Certificate of Registration</td>
+                      <td>Approved/Rejected</td>
+                      <td>BSU </td>
+                      <td>
+                        <div class="btn-group-vertical d-flex" >
+                          <!-- <button class="btn btn-primary" data-bs-toggle="modal">View File</button> -->
+                          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDialogScrollable"> View File</button>
+                            <div class="modal fade" id="modalDialogScrollable" tabindex="-1">
+                              <div class="modal-dialog modal-dialog-scrollable">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title">School ID</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                  </div>
+                                  <div class="modal-body">
+                                  <label class="control-label">Select File</label>
+                                    <input id="input-b5" name="input-b5[]" type="file" multiple>
+                                    <script>
+                                    $(document).ready(function() {
+                                        $("#input-b5").fileinput({showCaption: false, dropZoneEnabled: false});
+                                    });
+                                    </script>
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary">Save</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="Delete">Delete</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          <button class="btn btn-dark" data-bs-toggle="modal">Not Applicable</button>
+                          <!-- <button class="btn btn-warning" data-bs-toggle="modal">Save</button> -->
+                          <!-- <button class="btn btn-warning" data-toggle="modal" data-target="#editRoomModal">Edit</button>
+                          <button class="btn btn-danger" data-toggle="modal" data-target="#deleteRoomModal">Delete</button> -->
+                        </div>
+                      </td>
+                    </tr>
+                </tbody>
+              </table>
           </div>
         </div>
     </div>
