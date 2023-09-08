008f7aaead3b035e9fbe337903149993d3b71d57
valjuniel123 <valjuniel123@gmail.com>
2023-09-08 21:25:19 +0800

    Fix DB

--- account/model/dbconnection.php
+++ account/model/dbconnection.php
@@ -1,8 +1,18 @@
 <?php
+    if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
+        $dbuser     = 'root';
+        $dbpassword = '';
+        $dbdatabase = 'db_scholarsystem';
+    } else {
+        $dbuser     = 'u497506402_admin';
+        $dbpassword = 'Santotomas.0907';
+        $dbdatabase = 'u497506402_scholarsystem';
+    }
+
     $dbhost = 'localhost';
-    $dbuser = 'root';
-    $dbpassword = '';
-    $dbdatabase = 'db_scholarsystem';
+    // $dbuser = 'root';
+    // $dbpassword = '';
+    // $dbdatabase = 'db_scholarsystem';
     $dbport = 3306;
 
     $conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbdatabase, $dbport);
