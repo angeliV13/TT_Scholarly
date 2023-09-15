7d60d09fa0cec439efa612d81970a0349ea8d7c9
angeliV13 <angelireyes2213@gmail.com>
2023-09-15 20:36:05 +0800

    genReport

--- account/views/admin/generate_report.php
+++ account/views/admin/generate_report.php
@@ -21,18 +21,17 @@
                                     <h5 class="card-title">
                                         <i class="ri-account-circle-fill text-blue-50" style="font-size: 18px; margin-right: 8px;"></i>Applicant Report
                                     </h5>
-
                                     <div class="d-flex align-items-center">
                                         <!-- <button id="generate_ay" class="btn btn-sm btn-danger shadow-sm">
                                             <i class="fas fa-question fa-sm text-white-50 mr-2"></i>Generate Data
                                         </button> -->
-                                        <a class="collapsed mx-3" data-bs-target="#generate_report" data-bs-toggle="collapse" href="#">
+                                        <a class="collapsed mx-3" data-bs-target="#genrep_applicant" data-bs-toggle="collapse" href="#">
                                             <i class="bi bi-chevron-down ms-auto"></i>
                                         </a>
                                     </div>
                                 </div>
                                 <!-- Applicants Report Content -->
-                                <div id="generate_report" class="card-body">
+                                <div id="genrep_applicant" class="card-body">
                                     <div class="col-lg-12">
                                         <!-- Academic Year -->
                                         <div class="d-flex justify-content-between align-items-center">
@@ -41,13 +40,13 @@
                                         </div>
                                         <!-- Filter options -->
                                         <div class="row mb-1 mt-2 justify-content-between">
-                                            <form id="filterScholarType" class="column position-relative">
+                                            <form id="genScholarType" class="column position-relative">
 
                                                 <!-- Academic Year -->
                                                 <div class="row mb-3">
-                                                    <label for="filterScholarType" class="col-sm-5 col-form-label">Academic Year:</label>
+                                                    <label for="ScholarType" class="col-sm-5 col-form-label">Academic Year:</label>
                                                     <div class="col-sm-12" id="">
-                                                        <select class="form-select" id="filterScholarType">
+                                                        <select class="form-select" id="ScholarType">
                                                             <option selected value="">-- </option>
                                                             <option value="1">2020-2021</option>
                                                             <option value="2">2021-2022</option>
@@ -59,9 +58,9 @@
 
                                                 <!-- Scholarship Type -->
                                                 <div class="row mb-3">
-                                                    <label for="filterScholarType" class="col-sm-5 col-form-label">Scholarship Type:</label>
+                                                    <label for="ScholarType" class="col-sm-5 col-form-label">Scholarship Type:</label>
                                                     <div class="col-sm-12" id="">
-                                                        <select class="form-select" id="filterScholarType">
+                                                        <select class="form-select" id="ScholarType">
                                                             <option selected value="">-- </option>
                                                             <option value="1">Educational Assistance Program</option>
                                                             <option value="2">Full Scholarship Program</option>
@@ -71,52 +70,35 @@
 
                                                 <!-- Education Level -->
                                                 <div class="row mb-3">
-                                                    <label for="filterEducationLevel" class="col-sm-5 col-form-label font-bold">Educational Level:</label>
+                                                    <label for="educationLevel" class="col-sm-5 col-form-label font-bold">Educational Level:</label>
                                                     <div class="col-sm-12">
-                                                        <select class="form-select" id="filterEducationLevel">
+                                                        <select class="form-select" id="educationLevel">
                                                             <option selected value="">-- </option>
-                                                            <option value="1">Senior High School</option>
-                                                            <option value="1">College - Public</option>
                                                             <option value="1">College - Private</option>
+                                                            <option value="2">College - Public</option>
+                                                            <option value="2">Senior Highschool</option>
                                                         </select>
                                                     </div>
                                                 </div>
 
-                                                <!-- SCHOOL/UNIVERSITIES -->
+                                                <!-- School Name -->
                                                 <div class="row mb-3">
-                                                    <label for="filterYearLevel" class="col-sm-5 col-form-label font-bold">School Name:</label>
-                                                    <div id="yearLevelContainer" class="col-sm-12">
-                                                        <select class="form-select" id="filterYearLevel">
+                                                    <label for="schoolName" class="col-sm-5 col-form-label font-bold">School Name:</label>
+                                                    <div class="col-sm-12">
+                                                        <select class="form-select" id="SchoolName">
                                                             <option selected value="">-- </option>
-                                                            <option value="1">BSUr</option>
+                                                            <option value="1">BSU</option>
                                                             <option value="2">FAITH</option>
                                                             <option value="3">UP</option>
                                                         </select>
                                                     </div>
                                                 </div>
 
-                                                <!-- COURSE/STRAND -->
-                                                <div class="row mb-3">
-                                                    <label for="filterYearLevel" class="col-sm-5 col-form-label font-bold">Course/Strand :</label>
-                                                    <div id="yearLevelContainer" class="col-sm-12">
-                                                        <select class="form-select" id="filterYearLevel">
-                                                            <option selected value="">-- </option>
-                                                            <option value="1">1st Year</option>
-                                                            <option value="2">2nd Year</option>
-                                                            <option value="3">3rd Year</option>
-                                                            <option value="4">4th Year</option>
-                                                            <option value="5">5th Year</option>
-                                                            <option value="11">Grade 11</option>
-                                                            <option value="12">Grade 12</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-
                                                 <!-- Year Level -->
                                                 <div class="row mb-3">
-                                                    <label for="filterYearLevel" class="col-sm-5 col-form-label font-bold">Year Level:</label>
+                                                    <label for="yearLevel" class="col-sm-5 col-form-label font-bold">Year Level :</label>
                                                     <div id="yearLevelContainer" class="col-sm-12">
-                                                        <select class="form-select" id="filterYearLevel">
+                                                        <select class="form-select" id="yearLevel">
                                                             <option selected value="">-- </option>
                                                             <option value="1">1st Year</option>
                                                             <option value="2">2nd Year</option>
@@ -128,48 +110,51 @@
                                                         </select>
                                                     </div>
                                                 </div>
-                                                <!-- Scholarship Type -->
+
+                                                <!-- COURSE/STRAND -->
                                                 <div class="row mb-3">
-                                                    <label for="filterScholarType" class="col-sm-5 col-form-label">Application Status:</label>
+                                                    <label for="courseStrand" class="col-sm-5 col-form-label font-bold">Course/Strand :</label>
+                                                    <div id="courseStrandContainer" class="col-sm-12">
+                                                        <select class="form-select" id="courseStrand">
+                                                            <option selected value="">-- </option>
+                                                            <option value="1">BS Information Technology</option>
+                                                            <option value="2">2nd Computer Science</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                                <!-- Application Status -->
+                                                <div class="row mb-3">
+                                                    <label for="app_status" class="col-sm-5 col-form-label">Applicants Status:</label>
                                                     <div class="col-sm-12" id="">
-                                                        <select class="form-select" id="filterScholarType">
+                                                        <select class="form-select" id="app_status">
                                                             <option selected value="">-- </option>
-                                                            <option value="1"></option>
-                                                            <option value="2">Full Scholarship Program</option>
+                                                            <option value="1">Complete Requirements</option>
+                                                            <option value="2">Incomplete Requirements</option>
+                                                            <option value="3">Disqualified</option>
+                                                            <option value="4">Socioeconomic Background</option>
+                                                        <option value="5">Religious Affiliation</option>
                                                         </select>
                                                     </div>
                                                 </div>
 
-                                                <!-- Education Level -->
+                                                
+                                                <!-- Application Demographic -->
                                                 <div class="row mb-3">
-                                                    <label for="filterEducationLevel" class="col-sm-5 col-form-label font-bold">Applicants Demographic:</label>
-                                                    <div class="col-sm-12">
-                                                        <select class="form-select" id="filterEducationLevel">
-                                                            <option selected value="">-- </option>
-                                                            <option value="1">Senior High School</option>
-                                                            <option value="1">College - Public</option>
-                                                            <option value="1">College - Private</option>
-                                                        </select>
+                                                    <label for="app_demo" class="col-sm-9 col-form-label">Applicants Demographic:</label>
+                                                    <div class="col-sm-12" id="app_demo">
+                                                        <option selected value="">--</option>
+                                                        <option value="1">Gender</option>
+                                                        <option value="2">Disability Status</option>
+                                                        <option value="3">Race and Ethnicity</option>
+                                                        <option value="4">Socioeconomic Background</option>
+                                                        <option value="5">Religious Affiliation</option>
                                                     </div>
                                                 </div>
 
-
-
-                                                <!-- School Name -->
-                                                <div class="row mb-3">
-                                                    <label for="filterSchool" class="col-sm-5 col-form-label font-bold">Applicants Trends:</label>
-                                                    <div class="col-sm-12">
-                                                        <select class="form-select" id="filterSchool">
-                                                            <option selected value="">-- </option>
-                                                            <option value="1">BSU</option>
-                                                            <option value="2">FAITH</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-
-
+                                                <!-- Set Query Filter button -->
                                                 <div class="d-grid pt-3 gap-2 d-flex justify-content-end">
-                                                    <button id="setFilter" class="btn btn-sm btn-danger shadow-sm">Set Query Filter </button>
+                                                    <button id="generateReport" class="btn btn-sm btn-danger shadow-sm">Set Query Filter </button>
                                                 </div>
                                             </form>
                                         </div>
@@ -292,7 +277,8 @@
                                                         </select>
                                                     </div>
                                                 </div>
-                                                <!-- Scholarship Type -->
+
+                                                <!-- Application Status -->
                                                 <div class="row mb-3">
                                                     <label for="filterScholarType" class="col-sm-5 col-form-label">Application Status:</label>
                                                     <div class="col-sm-12" id="">
@@ -338,7 +324,7 @@
                                             </form>
                                         </div>
                                     </div>
-                                </div>  
+                                </div>
                             </div>
                         </div>
                     </div><!-- End Reports -->
