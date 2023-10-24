1bd29e2b482154579b4682e335e7d5dc9d1595aa
Christian Kyle Valdez <christianvaldez608@gmail.com>
2023-10-24 09:12:13 +0800

    fix

--- account/model/accountHandlerModel.php
+++ account/model/accountHandlerModel.php
@@ -274,7 +274,7 @@ function registerAccount($data)
 
             if ($query) 
             {
-                $sql = "INSERT INTO scholarship_application (ay_id, sem_id, userId, scholarType, account_type, dateApplied, status, current_active) VALUES ('$acadYear', '$semId', '$last_id', '$data[scholarType]', '1', NOW(), 0, 'info_flag')";
+                $sql = "INSERT INTO scholarship_application (ay_id, sem_id, userId, scholarType, account_type, dateApplied, status, current_active) VALUES ('$acadYear', '$semId', '$last_id', '$data[scholarType]', '3', NOW(), 0, 'info_flag')";
                 $query = mysqli_query($conn, $sql) or die("Error RQ004: " . mysqli_error($conn));
 
                 if ($query) 
