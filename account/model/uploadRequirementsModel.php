a1e9540f0eff999636eb069b1148e1e813bfc70c
valjuniel123 <valjuniel123@gmail.com>
2023-11-04 22:56:51 +0800

    Test

--- account/model/uploadRequirementsModel.php
+++ account/model/uploadRequirementsModel.php
@@ -128,9 +128,7 @@ function getAssessmentBeneTable($dashboard = 0)
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
                                                 <div class="modal-body">
-                                                    <object data="uploads/assessment/' . $file . '.pdf" type="application/pdf">>
-                                                        <embed id="previewSchoolId" src="uploads/assessment/' . $file . '.pdf" frameborder="0" width="100%" height="400px">
-                                                    </object>
+                                                    <embed id="previewSchoolId" src="uploads/assessment/' . $file . '.pdf" frameborder="0" width="100%" height="400px">
                                                 </div>
 
                                             </div>
@@ -154,9 +152,7 @@ function getAssessmentBeneTable($dashboard = 0)
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
                                                 <div class="modal-body">
-                                                    <object data="uploads/assessment/' . $file . '.pdf" type="application/pdf">>
-                                                        <embed id="previewSchoolId" src="uploads/assessment/' . $file . '.pdf" frameborder="0" width="100%" height="400px">
-                                                    </object>
+                                                    <embed id="previewSchoolId" src="uploads/assessment/' . $file . '.pdf" frameborder="0" width="100%" height="400px">
                                                 </div>
 
                                             </div>
@@ -584,7 +580,9 @@ function getApplicationTable($dashboard = 0)
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                     <div class="modal-body">
-                                        <embed id="previewSchoolId' . $requirement . '" src="uploads/application/' . $file . '.pdf" frameborder="0" width="100%" height="400px">
+                                        <object data=""uploads/application/' . $file . '.pdf"" type="application/pdf" frameborder="0" width="100%" height="600px"
+                                            <embed id="previewSchoolId' . $requirement . '" src="uploads/application/' . $file . '.pdf" frameborder="0" width="100%" height="400px">
+                                        </object>
                                     </div>
 
                                 </div>
