c52a374c90aac70b073566600cd916dc3dfba094
Christian Kyle Valdez <christianvaldez608@gmail.com>
2023-11-10 08:38:40 +0800

    fix

--- account/controller/filterHandler.php
+++ account/controller/filterHandler.php
@@ -10,7 +10,7 @@ if (isset($_REQUEST['action']))
                 "scholarType"       => [['0', ''], ['1', 'College Scholarship'], ['2', 'College Educational Assistance'], ['3', 'SHS Educational Assistance']],
                 "educationLevel"    => [['0', ''], ['1', 'Senior High School'], ['2', 'College']],
                 "school"            => getSchoolsDetailsArray(1, 4, 3, 0),
-                "yearLevel"         => [['0', ''], ['11', 'Grade 11'], ['12', 'Grade 12'], ['1', 'First Year'], ['2', 'Second Year'], ['3', 'Third Year'], ['4', 'Fourth Year'], ['5', 'Fifth Year']],
+                "yearLevel"         => [['', ''], ['11', 'Grade 11'], ['12', 'Grade 12'], ['1', 'First Year'], ['2', 'Second Year'], ['3', 'Third Year'], ['4', 'Fourth Year'], ['5', 'Fifth Year']],
                 "classType"         => [['0', ''], ['1', 'Public (Within Sto.Tomas, Batangas)'], ['2', 'Private (Within Sto.Tomas, Batangas)'], ['3', 'Public (Outside Sto.Tomas, Batangas)'], ['4', 'Private (Outside Sto.Tomas, Batangas)']],
             ];
 
