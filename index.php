189133d977950301428e092e9e601f319f0e79e7
angeliV13 <angelireyes2213@gmail.com>
2023-11-11 23:04:48 +0800

    landing page2

--- index.php
+++ index.php
@@ -201,196 +201,103 @@ ons/bootstrap-icons.css" rel="stylesheet">
     </section><!-- End About Section -->
 
     <!-- ======= SCHOLARSHIP Section ======= -->
-    <section id="scholarship" class="menu">
+    <section id="scholarship" class="menu section-bg">
       <div class="container" data-aos="fade-up">
-        <div class="section-header">
-          <h2>Scholarships</h2>
-          <h3><span>SCHOLARSHIP PROGRAM OFFERS</span></h3>
+        <div class="row justify-content-between">
+          <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center align-items-center mb-lg-0">
+            <div class="scholarship-header">
+              <h2>Scholarships Program Offers</h2>
+              <!-- <h1><span>SCHOLARSHIP PROGRAM OFFERS</span></h3> -->
+            </div>
+          </div>
+
+          <ul class="nav nav-tabs d-flex justify-content-center col-lg-12 mt-4 order-1 order-lg-2" data-aos="fade-up" data-aos-delay="200">
+            <li class="nav-item">
+              <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#general-req">
+                <h4>General Requirements</h4>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="tab" data-bs-target="#senior-high">
+                <h4>Educational Assistance Program</h4>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="tab" data-bs-target="#collegeEA">
+                <h4>Full Scholarship Program</h4>
+              </a>
+            </li>
+          </ul>
+
+          <div class="tab-content col-lg-12 order-1 order-lg-2 hero-img">
+            <!-- GENERAL REQUIREMENTS  -->
+            <div class="tab-pane fade" id="senior-high">
+              <div class="tab-header" data-aos="fade-up" data-aos-delay="100">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <h5 class="card-title">Senior High School</h5>
+                    <p><?= $website_scholar_text[0]['scholarText'] ?></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Educational Assistance Program -->
+            <div class="tab-pane fade" id="collegeEA">
+              <div class="tab-header" data-aos="fade-up" data-aos-delay="100">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class="card d-flex justify-content-center">
+                      <div class="card-body">
+                        <h5 class="card-title">College Educational Assistance</h5>
+                        <p><?= $website_scholar_text[1]['scholarText'] ?></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Full Scholarship Program -->
+            <div class="tab-pane fade" id="collegeFS">
+              <div class="tab-header text-center" data-aos="fade-up" data-aos-delay="100">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class="card d-flex justify-content-center">
+                      <div class="card-body">
+                        <h5 class="card-title">College Full Scholarship</h5>
+                        <p><?= $website_scholar_text[2]['scholarText'] ?></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="tab-pane fade active show" id="general-req">
+              <div class="tab-header text-center" data-aos="fade-up" data-aos-delay="100">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class="card d-flex justify-content-center">
+                      <div class="card-body">
+                        <h5 class="card-title">General Requirements</h5>
+                        <?php if ($gen_req != null) : ?>
+                          <?php foreach ($gen_req as $key => $req) : ?>
+                            <p><?= $req['req'] ?></p>
+                          <?php endforeach; ?>
+                        <?php else : ?>
+                          <p>No General Requirements for Now</p>
+                        <?php endif; ?>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <ul class="nav nav-tabs d-flex justify-content-center" data-aos="fade-up" data-aos-delay="200">
-          <li class="nav-item">
-            <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#general-req">
-              <h4>General Requirements</h4>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#senior-high">
-              <h4>Educational Assistance Program</h4>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#collegeEA">
-              <h4>Full Scholarship Program</h4>
-            </a>
-          </li>
-
-        </ul>
-
-        <div class="tab-content">
-          <!-- GENERAL REQUIREMENTS  -->
-          <div class="tab-pane fade" id="senior-high">
-            <div class="tab-header" data-aos="fade-up" data-aos-delay="100">
-              <div class="row">
-                <div class="col-lg-12">
-                  <h5 class="card-title">Senior High School</h5>
-                  <p><?= $website_scholar_text[0]['scholarText'] ?></p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Educational Assistance Program -->
-          <div class="tab-pane fade" id="collegeEA">
-            <div class="tab-header" data-aos="fade-up" data-aos-delay="100">
-              <div class="row">
-                <div class="col-lg-12">
-                  <div class="card d-flex justify-content-center">
-                    <div class="card-body">
-                      <h5 class="card-title">College Educational Assistance</h5>
-                      <p><?= $website_scholar_text[1]['scholarText'] ?></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Full Scholarship Program -->
-          <div class="tab-pane fade" id="collegeFS">
-            <div class="tab-header text-center" data-aos="fade-up" data-aos-delay="100">
-              <div class="row">
-                <div class="col-lg-12">
-                  <div class="card d-flex justify-content-center">
-                    <div class="card-body">
-                      <h5 class="card-title">College Full Scholarship</h5>
-                      <p><?= $website_scholar_text[2]['scholarText'] ?></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="tab-pane fade active show" id="general-req">
-            <div class="tab-header text-center" data-aos="fade-up" data-aos-delay="100">
-              <div class="row">
-                <div class="col-lg-12">
-                  <div class="card d-flex justify-content-center">
-                    <div class="card-body">
-                      <h5 class="card-title">General Requirements</h5>
-                      <?php if ($gen_req != null) : ?>
-                        <?php foreach ($gen_req as $key => $req) : ?>
-                          <p><?= $req['req'] ?></p>
-                        <?php endforeach; ?>
-                      <?php else : ?>
-                        <p>No General Requirements for Now</p>
-                      <?php endif; ?>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
       </div>
     </section><!-- End SCHOLARSHIP Section -->
-
-    <!-- ======= Details Section ======= -->
-    <section id="details" class="details">
-      <div class="container">
-
-        <div class="row content">
-          <div class="col-md-4" data-aos="fade-right">
-            <img src="assets/img/details-1.png" class="img-fluid" alt="">
-          </div>
-          <div class="col-md-8 pt-4" data-aos="fade-up">
-            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <ul>
-              <li><i class="bi bi-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="bi bi-check"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="bi bi-check"></i> Iure at voluptas aspernatur dignissimos doloribus repudiandae.</li>
-              <li><i class="bi bi-check"></i> Est ipsa assumenda id facilis nesciunt placeat sed doloribus praesentium.</li>
-            </ul>
-            <p>
-              Voluptas nisi in quia excepturi nihil voluptas nam et ut. Expedita omnis eum consequatur non. Sed in asperiores aut repellendus. Error quisquam ab maiores. Quibusdam sit in officia
-            </p>
-          </div>
-        </div>
-
-        <div class="row content">
-          <div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
-            <img src="assets/img/details-2.png" class="img-fluid" alt="">
-          </div>
-          <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
-            <h3>Corporis temporibus maiores provident</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
-            </p>
-            <p>
-              Inventore id enim dolor dicta qui et magni molestiae. Mollitia optio officia illum ut cupiditate eos autem. Soluta dolorum repellendus repellat amet autem rerum illum in. Quibusdam occaecati est nisi esse. Saepe aut dignissimos distinctio id enim.
-            </p>
-          </div>
-        </div>
-
-        <div class="row content">
-          <div class="col-md-4" data-aos="fade-right">
-            <img src="assets/img/details-3.png" class="img-fluid" alt="">
-          </div>
-          <div class="col-md-8 pt-5" data-aos="fade-up">
-            <h3>Sunt consequatur ad ut est nulla consectetur reiciendis animi voluptas</h3>
-            <p>Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus quia minima quod. Sunt saepe odit aut quia voluptatem hic voluptas dolor doloremque.</p>
-            <ul>
-              <li><i class="bi bi-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="bi bi-check"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="bi bi-check"></i> Facilis ut et voluptatem aperiam. Autem soluta ad fugiat.</li>
-            </ul>
-            <p>
-              Qui consequatur temporibus. Enim et corporis sit sunt harum praesentium suscipit ut voluptatem. Et nihil magni debitis consequatur est.
-            </p>
-            <p>
-              Suscipit enim et. Ut optio esse quidem quam reiciendis esse odit excepturi. Vel dolores rerum soluta explicabo vel fugiat eum non.
-            </p>
-          </div>
-        </div>
-
-        <div class="row content">
-          <div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
-            <img src="assets/img/details-4.png" class="img-fluid" alt="">
-          </div>
-          <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
-            <h3>Quas et necessitatibus eaque impedit ipsum animi consequatur incidunt in</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
-            </p>
-            <ul>
-              <li><i class="bi bi-check"></i> Et praesentium laboriosam architecto nam .</li>
-              <li><i class="bi bi-check"></i> Eius et voluptate. Enim earum tempore aliquid. Nobis et sunt consequatur. Aut repellat in numquam velit quo dignissimos et.</li>
-              <li><i class="bi bi-check"></i> Facilis ut et voluptatem aperiam. Autem soluta ad fugiat.</li>
-            </ul>
-          </div>
-        </div>
-
-      </div>
-    </section><!-- End Details Section -->
 
     <!-- ======= Testimonials Section ======= -->
     <section id="testimonials" class="testimonials section-bg">
