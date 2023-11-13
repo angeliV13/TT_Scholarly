0b29f02fe325dd4bb4a48c7fdd924bfcb79b4196
valjuniel123 <valjuniel123@gmail.com>
2023-11-13 15:14:32 +0800

    Printing Body

---
+++ account/assets/js/print.js
@@ -0,0 +1,9 @@
+function printData(tableId, header=0)
+{
+    let divToPrint=document.getElementById(tableId);
+
+    newWin= window.open("");
+    newWin.document.write(divToPrint.outerHTML);
+    newWin.print();
+    newWin.close();
+}
