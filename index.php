4adfc8a099a865630713786c63aad74d5ae21d42
angeliV13 <angelireyes2213@gmail.com>
2023-11-14 14:30:29 +0800

    changes1

--- index.php
+++ index.php
@@ -153,7 +153,7 @@
       </div>
     </div>
 
-    <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 30 150 30" preserveAspectRatio="none">
+    <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 28 150 30" preserveAspectRatio="none">
       <defs>
         <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
       </defs>
