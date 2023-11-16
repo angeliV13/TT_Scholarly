9df21d5acccbc924889be0fd3e3ccc57a04ee058
valjuniel123 <valjuniel123@gmail.com>
2023-11-17 00:51:51 +0800

    Push exam

--- account/views/applicants/exam_proper.php
+++ account/views/applicants/exam_proper.php
@@ -10,7 +10,7 @@
   </div>
   <div class="timer-top d-flex align-items-center justify-content-center active">
     <p id="time" class="mt-3">
-      <?php echo (isset($examAccess[2]) ? $examAccess[2] : "00:00:00" ); ?>
+      <?php echo (isset($examAccess[2]) ? $examAccess[2] : "00:00:00"); ?>
     </p>
   </div>
   <!-- End Page Title -->
@@ -18,27 +18,54 @@
   <div class="card">
 
     <form id="exam_container" class="prevent-select">
-      
+
     </form>
-    
+
   </div>
-  
+
   <!-- Instructions Modal -->
   <div class="modal fade" id="instructionsModal" tabindex="-1">
-        <div class="modal-dialog modal-fullscreen">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">General Directions / Instructions</h5>
-                </div>
-                <div class="modal-body">
-                    
-                </div>
-                <div class="modal-footer">
-                    <button id="start_exam" type="button" class="btn btn-warning">Start Exam</button>
-                    <button id="cancel_exam" type="button" class="btn btn-secondary">Cancel</button>
-                </div>
-            </div>
+    <div class="modal-dialog modal-fullscreen">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">General Directions / Instructions</h5>
         </div>
+        <div class="modal-body">
+          <!-- DECLARATION OF ACCURACY -->
+          <h6 class="fw-bold" style="font-size: 20px; color: #010483;"><i class="bi bi-shield-lock-fill" style></i> <span style="margin: 2px;">
+            Welcome to the scholarship examination. Please carefully read and follow the instructions below:
+          </h6>
+          <p class="small pb-3" style="text-align: justify;">
+            Please answer the all the examinations
+          </p>
+
+          <!-- AUTHORIZATION -->
+          <h6 class="fw-bold" style="font-size: 20px; color: #010483;"><i class="bi bi-shield-lock-fill" style></i> <span style="margin: 2px;">
+              Time limit:
+          </h6>
+          <p class="small pb-3" style="text-align: justify;">
+            The time limit is posted on the top right portion of the screen. 
+            In 1 minute, it will prompt that you are near the deadline
+            By reaching the deadline, your answers will be auto submitted.
+          </p>
+
+          <!-- Information Sourcing -->
+          <h6 class="fw-bold" style="font-size: 20px; color: #010483;"><i class="bi bi-shield-lock-fill" style></i> <span style="margin: 2px;">
+              Leaving:
+          </h6>
+          <p class="small pb-3" style="text-align: justify;">
+            Leaving the examination will marked you as finished.
+          </p>
+          <h6 class="fw-bold" style="font-size: 20px; color: #010483;"><i class="bi bi-shield-lock-fill" style></i> <span style="margin: 2px;">
+              Good luck!
+          </h6>
+        </div>
+        <div class="modal-footer">
+          <button id="start_exam" type="button" class="btn btn-warning">Start Exam</button>
+          <button id="cancel_exam" type="button" class="btn btn-secondary">Cancel</button>
+        </div>
+      </div>
     </div>
-  
+  </div>
+
 </main><!-- End #main -->
