99b8c4b27114c1291da059b719e23fbe3d120d5e
valjuniel123 <valjuniel123@gmail.com>
2023-11-16 08:00:48 +0800

    Test FB

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php include('account/model/functionModel.php') ?>
 <?php include('account/global_variables.php') ?>
-<?php header('Access-Control-Allow-Origin: *') ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -622,33 +621,8 @@
 
   <div id="preloader"></div>
 
-  <!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>
-window.fbAsyncInit = function() {
-  FB.init({
-    xfbml            : true,
-    version          : 'v18.0'
-  });
-};
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your Chat Plugin code -->
-<div class="fb-customerchat"
-  attribution="install_email"
-  attribution_version="biz_inbox"
-  page_id="691221147555210">
-</div>
-
-<!-- Messenger Chat Plugin Code -->
-<div id="fb-root"></div>
+  <!-- Messenger Chat Plugin Code -->
+  <div id="fb-root"></div>
 
 <!-- Your Chat Plugin code -->
 <div id="fb-customer-chat" class="fb-customerchat">
