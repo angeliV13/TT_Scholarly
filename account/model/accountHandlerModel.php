4099cce7881418b327b18e755f7489273b2b5d3b
Christian Kyle Valdez <christianvaldez608@gmail.com>
2023-11-20 22:52:59 +0800

    fix

--- account/model/accountHandlerModel.php
+++ account/model/accountHandlerModel.php
@@ -891,12 +891,12 @@ function updateFamilyInfo($data)
 
     $father = $mother = $spouse = $siblings = $guardian = 'success';
 
-    // if ($data['fatherArr'] != null) $father = updateFamily($data['fatherArr'], 0, $data['userId']);
-    // if ($father != 'success') return 'Update Family Father Error: ' . $father;
-    // if ($data['motherArr'] != null) $mother = updateFamily($data['motherArr'], 1, $data['userId']);
-    // if ($mother != 'success') return 'Update Family Mother Error: ' . $mother;
-    // if ($data['spouseArr']['firstName'] != '') $spouse = updateFamily($data['spouseArr'], 2, $data['userId']);
-    // if ($spouse != 'success') return 'Update Family Spouse Error: ' . $spouse;
+    if ($data['fatherArr'] != null) $father = updateFamily($data['fatherArr'], 0, $data['userId']);
+    if ($father != 'success') return 'Update Family Father Error: ' . $father;
+    if ($data['motherArr'] != null) $mother = updateFamily($data['motherArr'], 1, $data['userId']);
+    if ($mother != 'success') return 'Update Family Mother Error: ' . $mother;
+    if ($data['spouseArr']['firstName'] != '') $spouse = updateFamily($data['spouseArr'], 2, $data['userId']);
+    if ($spouse != 'success') return 'Update Family Spouse Error: ' . $spouse;
     if ($data['siblings'] != null) $siblings = updateFamily($data['siblings'], 3, $data['userId']);
     if ($siblings != 'success') return 'Update Family Sibling Error: ' . $siblings;
     if ($data['guardianArr']['firstName'] != '') $guardian = updateFamily($data['guardianArr'], 4, $data['userId']);
@@ -926,10 +926,11 @@ function updateFamily($data, $type, $userId)
             $middleName = (isset($nameArr[2])) ? $nameArr[2] : "";
             $lastName = (isset($nameArr[0])) ? $nameArr[0] : "";
 
-            $exists = check_exist_multiple(['table' => 'user_family', 'column' => ['user_id' => ['=', $userId], 'id' => ['=', $id], 'ay_id' => ['=', $acadYear], 'sem_id' => ['=', $defaultYear]]]);
+            $exists = 0;
+
+            if ($id != "") $exists = check_exist_multiple(['table' => 'user_family', 'column' => ['user_id' => ['=', $userId], 'id' => ['=', $id], 'ay_id' => ['=', $acadYear], 'sem_id' => ['=', $defaultYear]]]);
             // $exists = check_exist_multiple(['table' => 'user_family', 'column' => ['user_id' => ['=', $userId], 'id' => ['=', $id]]]);
 
-            return $exists;
             if ($exists > 0) 
             {
                 $sql = "UPDATE user_family SET firstName = '$firstName', middleName = '$middleName', lastName = '$lastName', age = '$age', occupation = '$occupation',
--- account/model/functionModel.php
+++ account/model/functionModel.php
@@ -1672,8 +1672,6 @@ function check_exist_multiple($data, $type = 0) // 0 - Num Rows, 1 - Data
         $sql = substr($sql, 0, -4);
     }
 
-    return $sql;
-
     $query = $conn->query($sql);
 
     if ($type == 0) 
