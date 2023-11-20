4f0973c6b1a85569b132588ebf28af3ac6e4fe94
valjuniel123 <valjuniel123@gmail.com>
2023-11-20 10:59:51 +0800

    Test Push

--- account/views/applicants/profile_applicant.php
+++ account/views/applicants/profile_applicant.php
@@ -363,7 +363,7 @@
                   <input type="hidden" id="elemId" value="<?= (!isset($education[3]['educ_id'])) ? "" : $education[3]['educ_id'] ?>">
 
                   <!-- COLLEGE LEVEL -->
-                  <?php $_SESSION['scholarType']; ?>
+                  <?php echo $_SESSION['scholarType']; ?>
                   <?php if ($_SESSION['scholarType'] == 1 || $_SESSION['scholarType'] == 2) : ?>
                     <!-- Custom Styled Validation with Tooltips -->
                     <?php if (isset($education[0])) : ?>
