ae69b8f3cb15e63cdcabd83690470131fcd0fc42
valjuniel123 <valjuniel123@gmail.com>
2023-11-21 08:21:48 +0800

    PUSH

--- account/assets/js/secure.js
+++ account/assets/js/secure.js
@@ -1,62 +1,62 @@
-$(document).keydown(function(e){ 
-    if(e.which === 123){ 
-        Swal.fire({
-            title: "Error!",
-            icon: "error",
-            html: "Dev Tools is not Possible",
-          }).then((result) => {
-            e.preventDefault();
-          });
-       return false; 
-    } if (e.ctrlKey && e.shiftKey && e.which == 'I') {
-        Swal.fire({
-            title: "Error!",
-            icon: "error",
-            html: "Dev Tools is not Possible",
-          }).then((result) => {
-            e.preventDefault();
-          });
-        e.preventDefault();
-    }
-    if (e.ctrlKey && e.shiftKey && e.which == 'C') {
-        Swal.fire({
-            title: "Error!",
-            icon: "error",
-            html: "Dev Tools is not Possible",
-          }).then((result) => {
-            e.preventDefault();
-          });
-        e.preventDefault();
-    }
-    if (e.ctrlKey && e.shiftKey && e.which == 'J') {
-        Swal.fire({
-            title: "Error!",
-            icon: "error",
-            html: "Dev Tools is not Possible",
-          }).then((result) => {
-            e.preventDefault();
-          });
-        e.preventDefault();
-    }
-    if (e.ctrlKey && e.key == 'U') {
-        Swal.fire({
-            title: "Error!",
-            icon: "error",
-            html: "Dev Tools is not Possible",
-          }).then((result) => {
-            e.preventDefault();
-          });
-        e.preventDefault();
-    }
+// $(document).keydown(function(e){ 
+//     if(e.which === 123){ 
+//         Swal.fire({
+//             title: "Error!",
+//             icon: "error",
+//             html: "Dev Tools is not Possible",
+//           }).then((result) => {
+//             e.preventDefault();
+//           });
+//        return false; 
+//     } if (e.ctrlKey && e.shiftKey && e.which == 'I') {
+//         Swal.fire({
+//             title: "Error!",
+//             icon: "error",
+//             html: "Dev Tools is not Possible",
+//           }).then((result) => {
+//             e.preventDefault();
+//           });
+//         e.preventDefault();
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.which == 'C') {
+//         Swal.fire({
+//             title: "Error!",
+//             icon: "error",
+//             html: "Dev Tools is not Possible",
+//           }).then((result) => {
+//             e.preventDefault();
+//           });
+//         e.preventDefault();
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.which == 'J') {
+//         Swal.fire({
+//             title: "Error!",
+//             icon: "error",
+//             html: "Dev Tools is not Possible",
+//           }).then((result) => {
+//             e.preventDefault();
+//           });
+//         e.preventDefault();
+//     }
+//     if (e.ctrlKey && e.key == 'U') {
+//         Swal.fire({
+//             title: "Error!",
+//             icon: "error",
+//             html: "Dev Tools is not Possible",
+//           }).then((result) => {
+//             e.preventDefault();
+//           });
+//         e.preventDefault();
+//     }
  
-});
+// });
 
-$("body").on("contextmenu", function(e){
-    Swal.fire({
-        title: "Error!",
-        icon: "error",
-        html: "Right clicking is not Possible",
-      }).then((result) => {
-        e.preventDefault();
-      });
-});
+// $("body").on("contextmenu", function(e){
+//     Swal.fire({
+//         title: "Error!",
+//         icon: "error",
+//         html: "Right clicking is not Possible",
+//       }).then((result) => {
+//         e.preventDefault();
+//       });
+// });
