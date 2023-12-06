8d3238ac2d7fe7debc1911ef02b465e073539c67
valjuniel123 <valjuniel123@gmail.com>
2023-12-06 22:20:17 +0800

    Test

--- account/model/uploadRequirementsModel.php
+++ account/model/uploadRequirementsModel.php
@@ -1104,6 +1104,7 @@ function setRequirements($userid, $id, $status, $state, $remarks){
             updateRequirementStatus($id, $status, 'renewal_file', $remarks);
             break;
     }
+    return $entries;
     if($entries->num_rows <> 0) {
         return (update_applicant_status($userid, 1) == true ? 'Success' : 'Error');
     }
